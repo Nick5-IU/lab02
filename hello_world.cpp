f3bd7ebe2a06966ef4474001c3d061e4df8f184d
Nick5-IU <mn1ck555@yandex.ru>
2025-06-07 20:44:54 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,10 @@
-   #include <iostream>
- #include <string>
- 
- using namespace std;
- 
- int main() {
-     string name;
-     cout << "Enter your name: ";
-     cin >> name;
-     cout << "Hello, World! from " << name << endl;
-     return 0;
- }
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+    std::cout << "Enter your name: "; // Запрос имени пользователя
+    std::cin >> name;
+    std::cout << "Hello, World! from " << name << std::endl; // Вывод приветствия
+    return 0;
+}
